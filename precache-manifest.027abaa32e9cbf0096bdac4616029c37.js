014118901fc22390268f5b9ef61bb4d086dc92a6
Nodirbek Sharipov <mail@nodir.dev>
2020-04-27 22:39:23 +0500

    initial

---
+++ precache-manifest.027abaa32e9cbf0096bdac4616029c37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de053502ed89287c70869c66afaea50b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d51f1a9f56ab56e9289b",
+    "url": "/static/css/main.23c9c000.chunk.css"
+  },
+  {
+    "revision": "1a79494d3ae121f5dd1a",
+    "url": "/static/js/2.a2abf0a9.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.a2abf0a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51f1a9f56ab56e9289b",
+    "url": "/static/js/main.6b2aa9da.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  }
+]);
